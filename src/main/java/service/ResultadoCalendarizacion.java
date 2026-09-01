4d3b83885159a615f569f8e26c93fb406fc5ec39
belenS-tech <smariabelen265@gmail.com>
2026-08-31 20:36:27 -0600

    calendarizacion
    se hizo la clase calendarizacion

--- src/main/java/service/ResultadoCalendarizacion.java
+++ src/main/java/service/ResultadoCalendarizacion.java
@@ -1,4 +1,28 @@
 package service;
 
+import model.FilaCalendarizacion;
+//import model.Recurso;
+
+/*import java.util.List;
 public class ResultadoCalendarizacion {
+    //private final List<Recurso> recursos;
+    private final List<FilaCalendarizacion> filas;
+
+    public ResultadoCalendarizacion(List<Recurso> recursos, List<FilaCalendarizacion> filas) {
+        this.recursos = recursos;
+        this.filas = filas;
+    }
+
+   public List<Recurso> getRecursos() {
+        return recursos;
+    }
+
+    public List<FilaCalendarizacion> getFilas() {
+        return filas;
+    }
+    public boolean estaVacio(){
+        return recursos.isEmpty();
+    }
 }
+
+ */
