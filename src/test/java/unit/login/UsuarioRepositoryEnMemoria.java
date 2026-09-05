7e3304b744da6fb1f7ca6dfadc85a31b32754ee6
Juan José Fernández <fernandezjuanjose42@gmail.com>
2026-09-05 16:01:28 -0600

    Pruebas unitarias de AuthService

---
+++ src/test/java/unit/login/UsuarioRepositoryEnMemoria.java
@@ -0,0 +1,36 @@
+package unit.login;
+
+import model.login.Usuario;
+import service.login.UsuarioRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioRepositoryEnMemoria implements UsuarioRepository {
+
+    private final List<Usuario> usuarios = new ArrayList<>();
+
+    @Override
+    public List<Usuario> listarTodos() {
+        return usuarios;
+    }
+
+    @Override
+    public Usuario buscarPorId(String id) {
+        return usuarios.stream()
+                .filter(u -> u.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public void guardar(Usuario usuario) {
+        usuarios.removeIf(u -> u.getId().equals(usuario.getId()));
+        usuarios.add(usuario);
+    }
+
+    @Override
+    public void eliminar(String id) {
+        usuarios.removeIf(u -> u.getId().equals(id));
+    }
+}
