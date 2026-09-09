5dc11316b62e5f8137b50cb553481efdc574912b
fioarcev <fiorella.arce.valverde@est.una.ac.cr>
2026-09-08 21:20:47 -0600

    Remove unnecessary stream operations in RecursoXmlRepository to simplify filtering logic

--- src/main/java/repository/RecursoXmlRepository.java
+++ src/main/java/repository/RecursoXmlRepository.java
@@ -16,7 +16,6 @@ import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class RecursoXmlRepository implements RecursoRepository {
 
@@ -167,8 +166,6 @@ public class RecursoXmlRepository implements RecursoRepository {
                         && categoriaId != null
                         && categoriaId.equals(recurso.getCategoria().getId()))
                 .toList();
-                .filter(r -> r.getCategoria() != null && categoriaId.equals(r.getCategoria().getId()))
-                .collect(java.util.stream.Collectors.toList());
     }
 
     @Override
